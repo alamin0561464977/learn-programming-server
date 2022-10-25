92a2d2df49309a1437689c0dffe8d86a88fed872
alamin0561464977 <amin23435678901315@gmail.com>
2022-10-25 22:21:45 +0300

    create course details api

--- index.js
+++ index.js
@@ -5,10 +5,22 @@ const prot = process.env.PORT || 5000;
 
 app.use(cors());
 
+const courses = require('./data/courses.json');
+
 app.get('/', (req, res) => {
     res.send('We will Learn Programming')
 });
 
+app.get('/courses', (req, res) => {
+    res.send(courses);
+});
+
+app.get('/course-detail/:id', (req, res) => {
+    const id = req.params.id;
+    const courseDetails = courses.find(course => course.id == id);
+    res.send(courseDetails);
+})
+
 app.listen(prot, () => {
     console.log('we will learn Programming', prot)
-})
+});
